b2036df5df814bf851562400dbcfed7d72653c97
EnzoMenegaldo <enzo.menegaldo@gmail.com>
2018-04-10 17:01:10 +0200

    Added possibility to grab with the Tracker, as intended

--- MusicBox/Assets/OurAssets/Scripts/Network/UnityNetwork/ObjectSpawner.cs
+++ MusicBox/Assets/OurAssets/Scripts/Network/UnityNetwork/ObjectSpawner.cs
@@ -6,6 +6,7 @@ public class ObjectSpawner : NetworkBehaviour {
     public GameObject steamVR;
     public GameObject cameraRig;
     public Camera lobbyCamera;
+    public GameObject grabber;
         
     SteamVR_ControllerManager controllerManager;
 
@@ -15,6 +16,9 @@ public class ObjectSpawner : NetworkBehaviour {
         base.OnStartServer();
         Debug.Log("On Start Server");
         controllerManager = steamVR.GetComponent<SteamVR_ControllerManager>();
+        GameObject[] toActivate = GameObject.FindGameObjectsWithTag("ServerSideOnly");
+        foreach (var o in toActivate)
+            o.GetComponent<MeshRenderer>().enabled = true;
         steamVR.SetActive(true);
         cameraRig.SetActive(true);
         lobbyCamera.gameObject.SetActive(false);
@@ -28,6 +32,7 @@ public class ObjectSpawner : NetworkBehaviour {
         foreach (var o in toActivate)
             o.GetComponent<MeshRenderer>().enabled = true;
         lobbyCamera.gameObject.SetActive(false);
+        grabber.SetActive(true);
     }
 
     public override void OnNetworkDestroy()
